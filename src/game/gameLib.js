9d1174c2f526cb91c2718e0bf24a9f9499634e50
ms0223900 <ms0223900@gmail.com>
2019-06-14 13:28:53 +0800

    190614_update bullet generate and check collision

--- src/game/gameLib.js
+++ src/game/gameLib.js
@@ -7,9 +7,10 @@ import {
 import { canvasSpec } from '../config'
 //
 export class BasicObj {
-  constructor({ id='basicObj', cloneId=0, x=0, y=0, width=100, height=100, fillStyle='#111', strokeStyle='#fff', collideObjs=[] }) {
+  constructor({ id='basicObj', cloneId=0, type='normal', x=0, y=0, width=100, height=100, fillStyle='#111', strokeStyle='#fff', collideObjs=[], movement=null }) {
     this.id = id
     this.cloneId = cloneId
+    this.type = type
     //
     this.x = x
     this.y = y
@@ -22,7 +23,7 @@ export class BasicObj {
       h: this.height,
     }
     //movement
-    this.movement = {
+    this.movement = movement || {
       isMove: false,
       vx: 4,
       vy: 4,
@@ -32,7 +33,12 @@ export class BasicObj {
     this.collideObjs = collideObjs
     //
     this.wall = {
-      spec: null
+      spec: {
+        x: 0,
+        y: 0,
+        w: canvasSpec.width,
+        h: canvasSpec.height,
+      }
     }
     this.fillStyle = fillStyle
     this.strokeStyle = strokeStyle
@@ -141,20 +147,29 @@ export class collidableObj extends BasicObj {
 }
 //
 
-const myBall = new Ball({ x: 440, y: 40, })
+const myBall = new Ball({ x: 440, y: 40, fillStyle: '#a00' })
 myBall.setProp('movement', {
   ...myBall.movement,
-  isMove: true,
+  isMove: false,
   vx: -10,
   vy: 10,
 })
-myBall.getWallCollide({
-  x: 0,
-  y: 0,
-  w: canvasSpec.width,
-  h: canvasSpec.height,
-})
 const myRect = new BasicObj({ x: 200, y: 0, width: 100, height: 100, fillStyle: '#3a0', collideObjs: [myBall] })
+
+const enemy = new BasicObj({ x: 300, y: 100, width: 20, height: 200, fillStyle: '#a90', })
+const getNewBullet = (x=0, y=0, newCloneId=0) => new BasicObj({ 
+  id: 'bullet', 
+  cloneId: newCloneId, 
+  x, y, 
+  width: 20, 
+  height: 20, 
+  fillStyle: '#f010ad', 
+  movement: {
+    isMove: true,
+    vx: 10, 
+    vy: 0,
+  } 
+})
 
 // console.log(myBall)
 
@@ -169,14 +184,74 @@ export class Layer {
   }
 }
 
+export class ControllableObj extends BasicObj {
+  constructor(props) {
+    super(props)
+    this.movement = {
+      ...this.movement,
+      vStandard: 4,
+      vx: 0,
+      vy: 0,
+      moveSet: [],
+    }
+    this.moveEvent()
+  }
+  moveEvent() {
+    console.log(this)
+    document.addEventListener('keydown', (e) => this.moveByUser(e))
+    document.addEventListener('keyup', (e) => {
+      const { keyCode } = e
+      this.movement.isMove = false
+      this.movement.moveSet = this.movement.moveSet.filter(m => m !== e)
+    })
+  }
+  moveByUser(e) {
+    const { keyCode } = e
+    this.movement.isMove = true
+    const getMoveSet = (moveset, keyCode) => {
+      if(moveset.includes(keyCode)) {
+        return moveset
+      } else {
+        return [...moveset, keyCode]
+      }
+    }
+    this.movement.moveSet = getMoveSet(this.movement.moveSet, keyCode)
+    console.log(this.movement.moveSet)
+    //move by keyCode
+    if(keyCode === 37) {
+      this.movement.vy = 0
+      this.movement.vx = this.movement.vStandard * -1
+    } else if(keyCode === 38) {
+      this.movement.vx = 0
+      this.movement.vy = this.movement.vStandard * -1
+    } else if(keyCode === 39) {
+      this.movement.vy = 0
+      this.movement.vx = this.movement.vStandard * 1
+      // this.x += 6
+    } else if(keyCode === 40) {
+      this.movement.vx = 0
+      this.movement.vy = this.movement.vStandard * 1
+    } else if(keyCode === 32) {
+      this.shootBullet()
+    }
+    console.log(keyCode)
+  }
+  shootBullet() {
 
-
+  }
+}
+const myPlayer = new ControllableObj({
+  fillStyle: '#a0a',
+  x: 100, y: 100,
+  w: 200, h: 200,
+})
 
 export class Game {
   constructor(canvas, canvasSpec, gameProp) {
     this.canvas = canvas
     this.canvasSpec = canvasSpec
     this.ctx = canvas.getContext('2d')
+    this.gameNewCloneId = 0
     // this.myLayers = myLayers
     this.gameProp = gameProp
     this.frame = { prev: 0, now: 0, fps: 0 }
@@ -185,6 +260,8 @@ export class Game {
       this.frame.prev = this.frame.now
       // console.log(this.frame)
     }, 500)
+    this.newGameObjs = []
+    document.addEventListener('keydown', (e) => this.newGameEvent(e))
   }
   setGameProp(prop, value) {
     this.gameProp = {
@@ -206,19 +283,41 @@ export class Game {
     this.ctx.fillRect(0, 0, this.canvasSpec.width, this.canvasSpec.height)
     this.ctx.restore()
   }
+  newGameEvent(e) {
+    const { keyCode } = e
+    if(keyCode === 32) {
+      this.newGameObjs = [
+        ...this.newGameObjs,
+        getNewBullet(myPlayer.x, myPlayer.y, this.gameNewCloneId), 
+      ]
+      this.gameNewCloneId += 1
+      console.log(this.newGameObjs)
+    }
+  }
   render() {
     this.ctx.clearRect(0, 0, this.canvasSpec.width, this.canvasSpec.height)
     //
-    
     this.ctx.beginPath()
     this.drawBG()
     this.drawFPS()
+    enemy.render(this.ctx)
     myRect.render(this.ctx)
+
     myBall.render(this.ctx)
+    for (let i = 0; i < this.newGameObjs.length; i++) {
+      const OBJ = this.newGameObjs[i]
+      if( simpleCheckObjCollide(OBJ, enemy) ) {
+        this.newGameObjs = this.newGameObjs.filter(o => o.cloneId !== OBJ.cloneId)
+      } else {
+        OBJ.render(this.ctx)
+      }
+      
+    }
+    myPlayer.render(this.ctx)
     const collideRes = simpleCheckObjCollide(myBall, myRect)
     if(collideRes) {
       this.setGameProp('score', this.gameProp.score + 1)
-      console.log(this.gameProp.score)
+      // console.log(this.gameProp.score)
     }
     this.ctx.closePath()
 
